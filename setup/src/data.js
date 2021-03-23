9d07b5f0364c9dbe9dfd9e01674333e4ac29b7d8
Bharatrathi27412 <56110278+Bharatrathi27412@users.noreply.github.com>
2021-03-23 17:04:45 +0530

    new project

---
+++ setup/src/data.js
@@ -0,0 +1,45 @@
+import instagram from './images/insta.png'
+import twitter from './images/tweet.png'
+import tele from './images/tele.jpg'
+import gmail from './images/gmail.png'
+
+const reviews = [
+  {
+    id: 1,
+    name: 'bharu_11',
+    job: 'Instagram',
+    image: 
+      instagram,
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'Bharathi27412',
+    job: 'Twitter',
+    image:
+      twitter,
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'Bharat Rathi',
+    job: 'Telegram',
+    image:
+      tele,
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'rathibharat27412@gmail.com',
+    job: 'E-mail',
+    image:
+      gmail,
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+export default reviews;
